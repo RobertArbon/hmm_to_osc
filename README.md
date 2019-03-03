330bb04720ed2367e6f2e5f17826aead63cdf6a2
Robert Arbon <robert.arbon@bristol.ac.uk>
2019-03-03 13:11:26 +0000

    Deleted cookier cutter setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# hmm_to_osc
-CLI to take a Pyemma HMM and turn it into osc message
--- src/hmm_to_osc/__init__.py
+++
@@ -1,5 +0,0 @@
-""" Package for the hmm_to_osc application.
-
-"""
-from .__version__ import __version__
-from .__main__ import main
